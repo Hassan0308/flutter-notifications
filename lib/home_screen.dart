c4716dd903f95251ffd95824f84660ce99d12228
hassan <hassankareem0315@gmail.com>
2024-10-03 11:26:35 +0500

    send notification to one to another device

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_notifications/access_token_firebase.dart';
 import 'package:flutter_notifications/local_notification_screen.dart';
 import 'package:flutter_notifications/notification_services.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,63 +19,84 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   NotificationServices notificationServices = NotificationServices();
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
   String? token;
+  AccessTokenFirebase accessToken = AccessTokenFirebase();
+
   @override
   void initState() {
-
     // TODO: implement initState
     super.initState();
-  //  notificationServices.checkAndRequestNotificationPermission();
+    //  notificationServices.checkAndRequestNotificationPermission();
     notificationServices.firebaseInit(context);
     notificationServices.setupInteractMessage(context);
-    notificationServices.getDeviceToken().then((value)  async {
-
+    notificationServices.getDeviceToken().then((value) async {
       setState(() {
-        token =  value;
+        token = value;
       });
 
       print(value);
-
-    }
-
-
-    );
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-
-    return  Scaffold(
-      body:Container(
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            GestureDetector(
-                onTap: () {
-                  Clipboard.setData(ClipboardData(text: token??"no"));
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Copied to clipboard')),
-                  );
+    return Scaffold(
+      body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              GestureDetector(
+                  onTap: () {
+                    Clipboard.setData(ClipboardData(text: token ?? "no"));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Copied to clipboard')),
+                    );
+                  },
+                  child: Text(token ?? "no")),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LocalNotificationScreen(
+                              flutterLocalNotificationsPlugin:
+                                  flutterLocalNotificationsPlugin)));
                 },
-                child: Text(token??"no")),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>LocalNotificationScreen(flutterLocalNotificationsPlugin: flutterLocalNotificationsPlugin)));
-              },
-              child: Text("Test Local Notifications"),
-            ),
-            ElevatedButton(
-              onPressed: (){
-              },
-              child: Text("Send Notification"),
-            ),
-          ],
-        )
-      ),
+                child: Text("Test Local Notifications"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  String token = "await accessToken.getAccessToken()";
+                  print(token);
+                  notificationServices.getDeviceToken().then((value) async {
+                    var data = {
+                      'message': {
+                        'token': value.toString(),
+                        'notification': {'title': 'Hassan', 'body': "Body"}
+                      }
+                    };
+                    await http.post(
+                        Uri.parse(
+                            "https://fcm.googleapis.com/v1/projects/flutter-notifications-3c7b8/messages:send"),
+                        body: jsonEncode(data),
+                        headers: {
+                          'Content-Type': 'application/json; charset=UTF-8',
+                          'Authorization': 'Bearer $token'
+                        }).then((value) {
+                          print(value.body);
+                    });
 
+                  });
+                },
+                child: Text("Send Notification"),
+              ),
+            ],
+          )),
     );
   }
 }
---
+++ lib/keys.dart
@@ -0,0 +1,19 @@
+class AppConstant{
+  static String private_key = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCcmB6h5ma53mhM\n6rLaOaRzCKlnfAWl1innUz8zRKjSuhEPfEZ8Jg0y62XsYyXZtwNon045o4A+b2lG\n63gKzq+iX4eOf/goOkBC9IwL6qmKhSjQAFRSriyZ2VXbPAmIeJZEAeCChaFjkX5L\nZgFcHwsYwQpbASpJ4FoclD19cPhzOcb5O0ZwE9WzUUMrRRaB6rMB3GDYPJF41+W9\nbu2QBFjCDeV9tZrpJjasf2Sa9jzmlHaMnLOYoJ/hA1i+Zy/+ijnO8GltTQAP8ZNs\nxGsTbAg2gd6i7PyS7YO49LaIJ2VCuQfVVjmCI/j8wXFoJLyG96UYkATyOg+CN4u3\nKJ2gVc1dAgMBAAECggEAD2s3EO/zy3dnw7MUW0Jzu9PtgkAG+lZFF/7taSnoiZ9C\nkMjG0/CXOh1NkuIMVwybLFELTceYmA02S4Jh9oZ1asKHQXO+Ro+9xhn4NDAt83K7\nGwf9RYra32cz+ftGYQamnRmmNpPb2Ub5LYlktuJjoFNZpq5NsKqDlO7Pwys9e3Lc\nXU4lUERqSV6ZkVujtGoP8AGKPujQwhCvC7nJrp/zPdeMCeq+cbqrnh2ed6MrczLf\nLukitmIE//TyjwH27dhDOjFDUS1UyBipbkrHjQu+Z33ac1SlJb2OqDjaKSnVaakx\nzxrm7ByN7lJMRM66NkwagiMX9+D8R4kfGYFHPj1vAQKBgQDaeftOY+JtATukq7Ud\nfiRdqPUE3FXNCS3CagzmGik8X2VIKAzwBKu31w5o9sHakN+n7DkfkJuRAQ/0Ectp\nw4Op1ZVnn9MXHh0ry66OVh1osElcKKfre/DLEw3FjiGaogsw8yaRvkGoSEvpXfD4\nZqxHC/6poYfB1x4vRYrTkdrVQQKBgQC3fUkl7l7uCj0skEGAA76LN+r1nR+1hkKC\n/z8P+S7c1wYBhr24AUaDFkt4ZeUoH0OmIW14rVdmoQitJ+L8tQH/UFXorFMnDZ8v\nN95J7BuWeEksirrK73JKr/+FvnnY7e9bpu2uOyhBHnPDL5Ko1Ml9njex60xNQD91\nzwcWXSllHQKBgFAB28YZThIdIXpsZyclyquB9DiTd9heRCOuMosRiYhSZwTFpfc/\nvqLUXrKRquwfocgc+c5VZqLzgKQg4B0bPFw1m0EXhnYw0P342TPG1X5BEJxvRw81\nlDjKAYEXS7cVIIQMh1NYGTNHRoVvoY9WCkD6Y4g7XzvdS3CERxKzlzABAoGACbOF\nD41X1YwTCe0imWsZQvEU6iGBkK5li9mE8wpAsQppVeFS3IystreSC+6PPBzTDdCM\noinACjwPYmBOfv/u+Bm+QQwDZXgx52Qzy4LBPqZjcAhxsdMRHuPJRb/5Xz/NPPJJ\ndMHJ8g9zHWnBfk2pzYt09GI1aYwxDsCAZm1Mh/0CgYEA155WEOJ7zV+VFMvePlje\nlrQnSdOTj1cGo6DvG065rIYNyjQvYx2v5VkmUCPReizwGjy3wjUJZbym05unR7IT\nFTk6QZELwkIDOYDOc0/mAgjaE+n8clweFMeE/qCw4/kxvq8BEc+DC+b6qUibTQgM\n4hAQCbSGUfvZqXpxF0jLzeM=\n-----END PRIVATE KEY-----\n";
+  static String private_key_id =  "5c66fb32352e93ab3b0ac115e22bc646ceca1cca";
+   static String client_id = "108596407471645378513";
+   static var data =    {
+     "type": "service_account",
+     "project_id": "flutter-notifications-3c7b8",
+     "private_key_id": private_key_id,
+     "private_key": private_key,
+     "client_email": "firebase-adminsdk-d7ja4@flutter-notifications-3c7b8.iam.gserviceaccount.com",
+     "client_id": client_id,
+     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+     "token_uri": "https://oauth2.googleapis.com/token",
+     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-d7ja4%40flutter-notifications-3c7b8.iam.gserviceaccount.com",
+     "universe_domain": "googleapis.com"
+   };
+
+}
